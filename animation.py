4f1263b975a71791a396a34e3c8ac6f5de115e31
SWADESH KUMAR NATH <78750873+SWADESH-KUMAR-NATH@users.noreply.github.com>
2021-05-05 02:01:25 +0530

    Delete animation.py

--- animation.py
+++
@@ -1,8 +0,0 @@
-def animate(i):
-    x, y = traj(i)
-    redDot.set_data(x,y)
-    return redDot,
-
-# create animation using the animate() function
-myAnimation = animation.FuncAnimation(fig, animate, interval=10, frames=np.arange(0.0, 100, .1), blit=True, repeat=False)
-plt.show()
